93e7ea5660173dc69cfdf0fb55502a46d0febd28
Kai Hotz <kaihotz@me.com>
2018-12-28 13:48:08 +0100

    Updates

---
+++ precache-manifest.ac577f6878af01c9bfbca7e14f2ed26f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c80b5e140d62bf48c849",
+    "url": "/React-Analog-Clock/static/js/runtime~main.c80b5e14.js"
+  },
+  {
+    "revision": "6fc568317c716cd759e0",
+    "url": "/React-Analog-Clock/static/js/main.6fc56831.chunk.js"
+  },
+  {
+    "revision": "e77f8e9500c725bcefb0",
+    "url": "/React-Analog-Clock/static/js/1.e77f8e95.chunk.js"
+  },
+  {
+    "revision": "6fc568317c716cd759e0",
+    "url": "/React-Analog-Clock/static/css/main.a97188f7.chunk.css"
+  },
+  {
+    "revision": "d4fd5e815c6faa1787592a85577803dc",
+    "url": "/React-Analog-Clock/index.html"
+  }
+];
