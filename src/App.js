dc27b842e42bb1d43423dd4e87d98b4fb14d7cb8
khotz <kai.hotz@complevo.de>
2019-02-11 16:41:57 +0100

    adjustments

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useInterval } from './hooks'
 import { ClockBase, Center, SecondHand, MinuteHand, HourHand } from './components'
 
 const App = () => {
+  const date = new Date();
   const [seconds, setSeconds] = useState(0)
   const [minutes, setMinutes] = useState(0)
   const [hours, setHours] = useState(0)
 
   useInterval(() => {
-    let date = new Date();
     setSeconds(date.getSeconds())
     setMinutes(date.getMinutes())
     setHours(date.getHours() % 12 || 12)
